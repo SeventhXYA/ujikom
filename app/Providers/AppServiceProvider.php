af6eeb48ee03cc52d5d7ce0757193a5621047d5d
Satria <satirakurniawan@gmail.com>
2023-07-20 12:26:18 +0800

    1042_Satria

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        //Memvalidasi bahwa validasi unique_nipt memiliki tugas untuk mengecek apakah nipt pada data.txt sudah ada atau belum (nipt tidak bileh sama)
         Validator::extend('unique_nipt', function ($attribute, $value, $parameters, $validator) {
-            $fileContent = Storage::disk('local')->exists('data.txt') ? Storage::disk('local')->get('data.txt') : '[]';
+            $fileContent = Storage::disk('local')->exists('data.txt') ? Storage::disk('local')->get('data.txt') : '[]'; //Cek apakah data.txt ada atau belum
             $data = json_decode($fileContent, true);
 
             foreach ($data as $item) {
